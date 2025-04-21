fe943aefc4be5d271b2b29e0ad2f9c9a3321fe45
vic990 <vicarballo03@gmail.com>
2025-04-21 17:03:30 -0600

    added the skeleton to MealCard

--- meal-finder/src/components/MainContent.tsx
+++ meal-finder/src/components/MainContent.tsx
@@ -1,14 +1,7 @@
-import {
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  Heading,
-  Image,
-  SimpleGrid,
-  Text,
-} from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import { Meal } from "../types";
+import MealCard from "./MealCard";
+import SkeletonCard from "./SkeletonCard";
 
 type Props = {
   meals: Meal[];
@@ -16,28 +9,11 @@ type Props = {
 };
 
 function MainContent({ meals, loading }: Props) {
-  console.log(meals, loading);
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
-    // Passing `columns={[2, null, 3]}` and `columns={{sm: 2, md: 3}}`
-    // will have the same effect.
-
     <SimpleGrid columns={[2, null, 3]} spacing="20px">
-      {meals.map((m) => (
-        <Card key={m.idMeal} boxShadow="lg">
-          <CardBody>
-            <Image src={m.strMealThumb} alt={m.strMeal} borderRadius="lg" />
-
-            <Heading size="md" color="blue.400">
-              <Text mt="4">{m.strMeal}a</Text>
-            </Heading>
-          </CardBody>
-          <CardFooter pt="0">
-            <Button color="white" bgColor="blue.400">
-              Check recipe
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+      {loading && skeletons.map((skeleton) => <SkeletonCard key={skeleton} />)}
+      {!loading && meals.map((m) => <MealCard key={m.idMeal} meal={m} />)}
     </SimpleGrid>
   );
 }
--- meal-finder/src/components/MealCard.tsx
+++ meal-finder/src/components/MealCard.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Heading,
+  Text,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react";
+import { Meal } from "../types";
 
-type Props = {};
+type Props = {
+  meal: Meal;
+};
 
-function MealCard({}: Props) {
-  return <div>MealCard</div>;
+function MealCard({ meal }: Props) {
+  return (
+    <Card boxShadow="lg">
+      <CardBody>
+        <Image src={meal.strMealThumb} alt={meal.strMeal} borderRadius="lg" />
+
+        <Heading size="md" color="blue.400">
+          <Text mt="4">{meal.strMeal}a</Text>
+        </Heading>
+      </CardBody>
+      <CardFooter pt="0">
+        <Button color="white" bgColor="blue.400">
+          Check recipe
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default MealCard;
